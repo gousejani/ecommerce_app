ae0ae53f022d547565a5cb05b78404a4c3e97667
Gouse Jani <gousejani13@gmail.com>
2021-02-12 10:07:20 +0530

    react-bootstrap setup, navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 const App = () => {
-	return <>Welcome to shopping chesko</>;
+	return (
+		<Fragment>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to Shopping Chesko</h1>{' '}
+				</Container>
+			</main>
+			<Footer />
+		</Fragment>
+	);
 };
 
 export default App;
