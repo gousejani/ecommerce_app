e1fc4467d67c6f3b39ffb6d0dbdaac910a814de9
Gouse Jani <gousejani13@gmail.com>
2021-02-13 18:40:45 +0530

    register route created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,4 +40,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 })
 
-export { authUser, getUserProfile }
+// @desc    Register a new User
+// @route   POST  /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body
+	const userExists = await User.findOne({ email })
+	if (userExists) {
+		res.status(400)
+		throw new Error('User Already Exists')
+	}
+	const user = await User.create({
+		name,
+		email,
+		password,
+	})
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid User Data')
+	}
+})
+
+export { authUser, getUserProfile, registerUser }
